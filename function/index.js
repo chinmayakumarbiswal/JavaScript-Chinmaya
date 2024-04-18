4f4956bb08f04b128f46642160012c88ea29ba0d
Chinmaya Kumar Biswal <chinmaya.kumar@ivalue.co.in>
2024-04-18 14:17:26 +0530

    function example

--- function/index.js
+++ function/index.js
@@ -1,3 +1,4 @@
+
 var x=1;
 a();
 b();
@@ -14,3 +15,6 @@ function b() {
     console.log(x)
 }
 
+// A JavaScript function is a block of reusable code that performs a specific task. 
+// It allows you to group related code together and execute it whenever needed. 
+// Functions are an essential part of JavaScript programming and are used to organize and modularize your code.
